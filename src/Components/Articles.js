23075089471802906a50abafbebf3d9aa2f2fcbb
Sean de Groot <degroot.sd@gmail.com>
2020-06-20 18:36:27 -0400

    Added stateless component

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from 'react'; 
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Articles = ({title, imgSrc, description, url}) => {
+const Articles = ({title, imgSrc, publishedAt, url}) => {
   return (
-    <article>
-      <h1>{title}</h1>
-      <img src={imgSrc} alt={title}/>
-      <p>{description}</p>
-      <a href={url}>Full Link</a>
-    </article>
-  )
+    <form action={url}>
+        <div className="image-container">
+        <img src={imgSrc} alt={title}/>
+        <h2>{title}</h2>
+        {/* <p>{publishedAt}</p> */}
+        <button aria-label={`${title}, enter to read more.`} title={url}><FontAwesomeIcon icon={faExternalLinkAlt} /></button>
+        </div>
+    </form>
+    )
 }
 
 export default Articles
