cdbd67810330a70391978cd8f16618a73e90454d
Sean de Groot <degroot.sd@gmail.com>
2020-06-20 18:37:15 -0400

    Changed query's state and passed to axios call as an argument

--- src/App.js
+++ src/App.js
@@ -4,57 +4,51 @@ import Articles from './Components/Articles';
 import axios from 'axios';
 import './index.scss';
 
-// User types out query and hits enter
-// Header.js component grabs input’s state and props to App.js
-// App.js takes state from Header.js, updates its state and puts it into the Axios call
-// Map through data grabbing article information relating to that parameter
-// Present on page through a stateless component Articles.js
-
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
       articles: [],
-      isLoading: true,
+      query: '',
+      isLoading: false
     }
   }
 
-  componentDidMount() {
+  handleSearch = value => {
     axios({
-      url: 'http://newsapi.org/v2/everything?q=apple&sortBy=popularity&apiKey=eb43cb932e264320adfd1b7942970622',
+      url: `https://newsapi.org/v2/everything?q=${value}&language=en&pageSize=100&apiKey=eb43cb932e264320adfd1b7942970622`,
       method: 'GET',
       responseType: 'JSON',
-      params: {
-        query: '',
-      },
-    }).then((response => {
-      let articles = response.data.articles;
+     }).then(response => {
+       let articles = response.data.articles;
 
-      this.setState({
-        articles,
-        isLoading: false,
-      })
-
-      console.log(articles);
-    }))
-  }
+       const filterArticleTitles = articles.filter((article, index, array) => {
+         return array.findIndex(secondIndex => (secondIndex.title === article.title)) === index
+       });
+       
+       this.setState({
+         articles: filterArticleTitles,
+         isLoading: false,
+         query: ''
+       })
+     }
+  )}
 
   render() {
-    const {isLoading, articles} = this.state;
+    const {articles} = this.state;
     return (
       <>
-        <Header />
+        <Header handleSearch={this.handleSearch}/>
         <div className="wrapper">
           <main>
             {
-              isLoading ? <p>Loading...</p> : articles.map((article, index) => {
+              articles.map((article, index) => {
                 return (
                   <Articles
                     key={index}
-                    title={article.title}
                     imgSrc={article.urlToImage}
-                    description={article.description}
+                    title={article.title}
+                    date={article.publishedAt}
                     url={article.url}
                   />
                 )
