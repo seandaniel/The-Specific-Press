1d7eec0707a059f0760b95e9b7a9f48eca1f5faf
Sean de Groot <degroot.sd@gmail.com>
2020-06-23 16:55:02 -0400

    Changed Error.js component name

---
+++ src/Components/NoResults.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { faThumbsDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import '../index.scss';
+
+const NoResults = () => {
+  return (
+    <div className="error-container">
+      <FontAwesomeIcon icon={faThumbsDown} />
+      <h3>That subject must not be newsworthy, try again.</h3>
+    </div>
+  )
+}
+
+export default NoResults
