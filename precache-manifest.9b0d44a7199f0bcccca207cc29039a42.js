43a7d2e549b002154d43aeaf2a2d87040d814a9b
Sean de Groot <degroot.sd@gmail.com>
2020-06-24 11:51:38 -0400

    Updates

---
+++ precache-manifest.9b0d44a7199f0bcccca207cc29039a42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a951dd05ed293ad278c8be1d18134aa4",
+    "url": "/the-specific-press/index.html"
+  },
+  {
+    "revision": "6a860181f72c19367cd0",
+    "url": "/the-specific-press/static/css/main.b4fed4aa.chunk.css"
+  },
+  {
+    "revision": "eb619606c742bc505028",
+    "url": "/the-specific-press/static/js/2.3f08bb42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-specific-press/static/js/2.3f08bb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a860181f72c19367cd0",
+    "url": "/the-specific-press/static/js/main.25572124.chunk.js"
+  },
+  {
+    "revision": "a52157b70c5a9d766f4a",
+    "url": "/the-specific-press/static/js/runtime-main.83d25b41.js"
+  }
+]);
