fb834f6a56e8ae1402a4131d35939252b76ca155
Sean de Groot <degroot.sd@gmail.com>
2020-07-08 09:05:37 -0400

    Changed favicon

--- src/Components/LoadingAnimation.js
+++ src/Components/LoadingAnimation.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faSync } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const LoadingAnimation = () => {
   return (
     <div className="loading-container">
-      <FontAwesomeIcon icon={faSpinner} aria-hidden="true"/>
+      <FontAwesomeIcon icon={faSync} aria-hidden="true"/>
     </div>
   )
 }
