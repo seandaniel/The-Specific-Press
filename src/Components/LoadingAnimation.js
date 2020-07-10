ca563a9eecd308bd83b411e936257503aba56893
Sean de Groot <degroot.sd@gmail.com>
2020-07-10 10:50:42 -0400

    Changed LoadingAnimation

--- src/Components/LoadingAnimation.js
+++ src/Components/LoadingAnimation.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { faSync } from "@fortawesome/free-solid-svg-icons";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const LoadingAnimation = () => {
   return (
     <div className="loading-container">
-      <FontAwesomeIcon icon={faSync} aria-hidden="true"/>
+      <FontAwesomeIcon icon={faSpinner} aria-hidden="true"/>
     </div>
   )
 }
